9a2c3d33e70e7ea84e5587f2eac41c87b60a05f0
Walker Hale IV <walker.hale.iv@gmail.com>
2013-02-11 20:12:46 -0600

    Simplified UTF-8 fix

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask.ext.wtf import Form, TextField, TextAreaField, PasswordField, \
 Required, ValidationError
 from flask.ext.login import LoginManager, login_required, current_user, \
 login_user, logout_user
-from utils import smart_str
 
 
 """
@@ -42,7 +41,8 @@ class Page(object):
 			os.makedirs(folder)
 		with open(self.path, 'w') as f:
 			for key, value in self._meta.items():
-				f.write(smart_str('%s: %s\n' % (key, value)))
+				line = u'%s: %s\n' % (key, value)
+				f.write(line.encode('utf-8'))
 			f.write('\n'.encode('utf-8'))
 			f.write(self.body.encode('utf-8'))
 		if update:
