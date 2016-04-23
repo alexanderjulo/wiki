dcb549c021dcff3792c8a145c1aa8da96274e063
Jan Cermak <me@johniez.cz>
2016-04-23 19:50:22 +0200

    #50: Do not allow to rename page outside content directory.

--- app.py
+++ app.py
@@ -241,10 +241,21 @@ class Wiki(object):
         return Page(path, url, new=True)
 
     def move(self, url, newurl):
-        os.rename(
-            os.path.join(self.root, url) + '.md',
-            os.path.join(self.root, newurl) + '.md'
-        )
+        source = os.path.join(self.root, url) + '.md'
+        target = os.path.join(self.root, newurl) + '.md'
+        # normalize root path (just in case somebody defined it absolute,
+        # having some '../' inside) to correctly compare it to the target
+        root = os.path.normpath(self.root)
+        # get root path longest common prefix with normalized target path
+        common = os.path.commonprefix((root, os.path.normpath(target)))
+        # common prefix length must be at least as root length is
+        # otherwise there are probably some '..' links in target path leading
+        # us outside defined root directory
+        if len(common) < len(self.root):
+            raise RuntimeError(
+                'Possible write attempt outside content directory: '
+                '%s' % newurl)
+        os.rename(source, target)
 
     def delete(self, url):
         path = self.path(url)
