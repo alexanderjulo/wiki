2169262a3210c74286d1b44d8438c662d806a86f
Alexander Jung-Loddenkemper <alexander@julo.ch>
2016-11-19 15:28:05 +0100

    release version 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wiki2',
-    version='2.0.0',
+    version='2.0.1',
     description='simple python markdown wiki with web ui',
     author='Alexander Jung-Loddenkemper',
     author_email='alexander@julo.ch',
