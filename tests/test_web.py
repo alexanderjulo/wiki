1c29e066162da4ed547862e59fb5f56406964601
Alexander Jung-Loddenkemper <alexander@julo.ch>
2020-11-02 18:19:48 +0100

    fix #83: add test to verify auth works

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,6 +1,15 @@
 from . import WikiBaseTestCase
 
 
+CONFIGURATION_PRIVATE = u"""
+PRIVATE=True
+TITLE='test'
+DEFAULT_SEARCH_IGNORE_CASE=False
+DEFAULT_AUTHENTICATION_METHOD='hash'
+SECRET_KEY='test'
+"""
+
+
 class WebContentTestCase(WikiBaseTestCase):
     """
         Various test cases around web content.
@@ -14,3 +23,20 @@ class WebContentTestCase(WikiBaseTestCase):
         rsp = self.app.get('/')
         assert b"You did not create any content yet." in rsp.data
         assert rsp.status_code == 200
+
+
+class AuthenticationTestCase(WikiBaseTestCase):
+    """
+        Test cases around authentication.
+    """
+
+    config_content = CONFIGURATION_PRIVATE
+
+    def test_auth_protection_works(self):
+        """
+            Assert that if auth is required and user is not
+            authenticated the request fails.
+        """
+        rsp = self.app.get('/')
+        assert rsp.status_code == 302
+        assert rsp.headers['Location'] == "http://localhost/user/login/?next=%2F"
