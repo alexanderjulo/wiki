7a4ed5afa9cc4d8240e1e4f846ec7433e4637c38
Eugene <ti.eugene@gmail.com>
2021-03-21 18:05:12 +0300

    Fix #91, #92, #93

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ setup(
     author='Alexander Jung-Loddenkemper',
     author_email='alexander@julo.ch',
     url='https://github.com/alexanderjulo/wiki',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     python_requires='>3.7',
     install_requires=[
         'Flask>=0.9',
-        'Click>=6,<7',
+        'Click>=6',
         'Flask-Login>=0.4',
         'Flask-WTF>=0.8',
         'Markdown>=2.2.0',
-        'Pygments>=1.5',
         'WTForms>=1.0.2',
         'Werkzeug>=0.8.3',
         'python-markdown-math'
